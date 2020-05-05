ec1abadf54219a455f77c41e26123b057d0464c2
callumgillies <callumgillies@nhs.net>
2020-05-05 16:02:40 +0100

    Passes pep8 test

--- MRI_Tag_Fix.py
+++ MRI_Tag_Fix.py
@@ -2,14 +2,14 @@ import easygui
 import pydicom
 import os
 
-selected_directory = easygui.diropenbox("Please select the folder containing the MRI DICOM Files")
+dir = easygui.diropenbox("Please select the DICOM directory")
 
-DCM_file_list = os.listdir(selected_directory)
+dicom_file_list = os.listdir(dir)
 
-DCM_path_list=[]
+dicom_path_list = []
 
-for filename in DCM_file_list:
-        if "MR" in filename.upper(): # Won't work if filenames don't start with MR
-            DCM = pydicom.read_file(os.path.join(selected_directory+'\\',filename))
-            DCM.ImageType = DCM.ImageType.replace(' ','\\')
-            DCM.save_as(os.path.join(selected_directory+'\\',filename))
+for filename in dicom_file_list:
+    if filename.startswith('MR'):
+        dicom = pydicom.read_file(os.path.join(dir + '\\', filename))
+        dicom.ImageType = dicom.ImageType.replace(' ', '\\')
+        dicom.save_as(os.path.join(dir + '\\', filename))
