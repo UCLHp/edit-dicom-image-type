b842128c898f00b4f5d306ab60f0f9d982a15a1c
callumgillies <callumgillies@nhs.net>
2020-05-05 17:14:51 +0100

    unfinished commit

--- MRI_Tag_Fix.py
+++ MRI_Tag_Fix.py
@@ -3,13 +3,19 @@ import pydicom
 import os
 
 dir = easygui.diropenbox("Please select the DICOM directory")
+print(f'Selected directory is: {dir}\n')
+
+if not dir:
+    print('No directory selected:')
+    print('Code terminated')
+    exit()
 
 dicom_file_list = os.listdir(dir)
-
-dicom_path_list = []
-
+counter=0
 for filename in dicom_file_list:
     if filename.startswith('MR'):
-        dicom = pydicom.read_file(os.path.join(dir + '\\', filename))
-        dicom.ImageType = dicom.ImageType.replace(' ', '\\')
-        dicom.save_as(os.path.join(dir + '\\', filename))
+        counter += 1
+        # dicom = pydicom.read_file(os.path.join(dir + '\\', filename))
+        # dicom.ImageType = dicom.ImageType.replace(' ', '\\')
+        # dicom.save_as(os.path.join(dir + '\\', filename))
+print(f'{counter} out of {len(dicom_file_list)} files identified as MRI images')
